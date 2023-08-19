da2fd2b5877d38637a45150e38f5164876e4158a
Daewoon Kim <me@kdw.kr>
2023-08-19 15:22:35 +0900

    Slow Village

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  const name = 'Junction Asia 2023';
+
+  return <>HELLO {name}</>;
+};
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLElement);
+root.render(createElement(App));
