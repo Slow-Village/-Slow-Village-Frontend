e50468e8fac122c26c80eda57a31a8ed42549ba0
Daewoon Kim <me@kdw.kr>
2023-08-20 02:29:59 +0900

    build: basename

--- src/router.tsx
+++ src/router.tsx
@@ -6,27 +6,32 @@ import MainPage from '~pages/MainPage';
 import ProfilePage from '~pages/ProfilePage';
 import ReviewPage from '~pages/ReviewPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainPage />,
+    },
+    {
+      path: '/details/:id',
+      element: <DetailPage />,
+    },
+    {
+      path: '/reviews/:id',
+      element: <ReviewPage />,
+    },
+    {
+      path: '/confirm-order/:id',
+      element: <ConfirmOrderPage />,
+    },
+    {
+      path: '/profile',
+      element: <ProfilePage />,
+    },
+  ],
   {
-    path: '/',
-    element: <MainPage />,
-  },
-  {
-    path: '/details/:id',
-    element: <DetailPage />,
-  },
-  {
-    path: '/reviews/:id',
-    element: <ReviewPage />,
-  },
-  {
-    path: '/confirm-order/:id',
-    element: <ConfirmOrderPage />,
-  },
-  {
-    path: '/profile',
-    element: <ProfilePage />,
-  },
-]);
+    basename: '/Slow-Village-Frontend',
+  }
+);
 
 export default router;
